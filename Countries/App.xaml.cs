d82bc118f5257d42e9eca87351c8697f2c14a1aa
theoneandholymoses <mosheb11234@gmail.com>
2024-09-04 19:58:18 +0300

    Add project files.

---
+++ Countries/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Countries
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Countries/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Countries;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Countries
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Country> Countries { get; set; } = new ObservableCollection<Country>();
+        private ObservableCollection<Country> _allCountries = new ObservableCollection<Country>();
+
+        public static HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadCountriesDataAsync();
+
+        }
+
+        private async Task LoadCountriesDataAsync()
+        {
+            string json = await client.GetStringAsync("https://restcountries.com/v3.1/all");
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            Countries = JsonSerializer.Deserialize<ObservableCollection<Country>>(json, options);
+
+            foreach (Country c in Countries)
+            {
+                _allCountries.Add(c);
+            }
+            CountriesDataGrid.ItemsSource = Countries;
+
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = SearchTextBox.Text.ToLower();
+            List<Country> filteredCountries = _allCountries
+                .Where(c => c.Name.Common.ToLower().Contains(searchText))
+                .ToList();
+
+            UpdateCountriesCollection(filteredCountries);
+        }
+
+        private void RegionFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedRegion = (RegionFilterComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if (selectedRegion == "All Regions")
+            {
+                UpdateCountriesCollection(_allCountries.ToList());
+            }
+            else
+            {
+                List<Country> filteredCountries = _allCountries
+                    .Where(c => c.Region.ToLower() == selectedRegion.ToLower())
+                    .ToList();
+
+                UpdateCountriesCollection(filteredCountries);
+            }
+        }
+
+
+
+        private void UpdateCountriesCollection(List<Country> countries)
+        {
+            Countries.Clear();
+            foreach (Country country in countries)
+            {
+                Countries.Add(country);
+            }
+        }
+    }
+}
